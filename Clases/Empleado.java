a5bfa6e4994acf280e9ae3fca34fc412b85c101c
joaquinperez04 <164825934+joaquinperez04@users.noreply.github.com>
2024-11-22 17:05:14 -0300

    Clase empleados

---
+++ Clases/Empleado.java
@@ -0,0 +1,30 @@
+package Clases;
+
+public class Empleado {
+
+    private int legajo;
+    private String rol;
+
+    public Empleado() {}
+
+    public Empleado(int legajo, String rol) {
+        this.legajo = legajo;
+        this.rol = rol;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+}
