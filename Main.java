c5374ff8655adb288e0fa9409225175ee982cd5c
PIPICBA <lichimedina05@gmail.com>
2024-11-22 16:42:43 -0300

    Login

--- Main.java
+++ Main.java
@@ -1,13 +1,3 @@
-import javax.swing.*;
-
 public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Ejemplo Swing");
-        JButton button = new JButton("Haz clic aquí");
-        button.setBounds(50, 50, 200, 50);
-        frame.add(button);
-        frame.setSize(400, 400);
-        frame.setLayout(null);
-        frame.setVisible(true);
-    }
+    
 }
