8a1f563550ac653bc8c4cc332afb503d841465e1
Ulises Francioni <ulisesfrancioni@gmail.com>
2024-11-22 19:16:29 -0300

    new MainForm

--- Forms/MainForm/MainForm.java
+++ Forms/MainForm/MainForm.java
@@ -1,20 +1,29 @@
 package Forms.MainForm;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
-    private JPanel mainPanel;
-    private JButton RegistrarRevisionButton;
 
     public MainForm(){
-        setContentPane(mainPanel);
         setTitle("Sistema VTV");
-        setSize(300, 300);
+        setSize(300, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        JButton registrarRevisionButton = new JButton("Registrar Revision");
+        registrarRevisionButton.setPreferredSize(new Dimension(200, 50));
+        add(registrarRevisionButton, gbc);
+
         setVisible(true);
-        RegistrarRevisionButton.addActionListener(new ActionListener() {
+
+        registrarRevisionButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
