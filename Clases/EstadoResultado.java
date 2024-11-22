cc8bb77b3efbab27c3b69e10472603c6ed60c3cd
AgustinFig <Figueroaagustin555@gmail.com>
2024-11-22 17:28:08 -0300

    Update and rename EstadoResultado to EstadoResultado.java

---
+++ Clases/EstadoResultado.java
@@ -0,0 +1,13 @@
+package Clases;
+
+public class EstadoResultado {
+  private String nombre;
+
+  public Persona(String nombre) {
+      this.nombre = nombre;
+  }
+  public String get.Nombre() {
+    retung nombre;
+  }
+  public void set Nombre(String nombre) {
+    this.nombre = nombre;
